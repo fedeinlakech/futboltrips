4623775f82ad44457826635d1c0647a542d4732c
Fede <fede.in.lakech@gmail.com>
2021-03-08 15:49:20 -0300

    Desafío Complementario Clase 6

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,168 @@
-// DESAFÍO CLASE 6
+// - - - - - - - DESAFÍO COMPLEMENTARIO CLASE 6 - - - - - - -
+
+
+// 1) Productos
+
+var productos = [
+    { nombre: "martillo", precio: 800 },
+    { nombre: "clavo", precio: 3 },
+    { nombre: "tornillo", precio: 5 },
+    { nombre: "llave", precio: 1300 },
+    { nombre: "destornillador", precio: 300 },
+    { nombre: "pinza", precio: 500 },
+    { nombre: "arandela", precio: 10 },
+];
+
+function ordenar_precio(arreglo, criterio) {
+    if (criterio == "A")
+        console.log(arreglo.sort(
+            function(a, b) {
+                return a.precio - b.precio;
+            }
+        ));
+
+    if (criterio == "D")
+        console.log(arreglo.sort(
+            function(a, b) {
+                return b.precio - a.precio;
+            }
+        ));
+}
+
+ordenar_precio(productos, "A");
+
+
+// 2) Personas
+
+var personas = [
+    { nombre: "Luis", edad: 45 },
+    { nombre: "Cecilia", edad: 12 },
+    { nombre: "Maximiliano", edad: 3 },
+    { nombre: "Arya", edad: 31 },
+    { nombre: "Raúl", edad: 44 },
+    { nombre: "Esteban", edad: 26 },
+    { nombre: "Marcela", edad: 62 },
+];
+
+function ordenar_edad(arreglo, criterio) {
+    if (criterio == "A")
+        console.log(arreglo.sort(
+            function(a, b) {
+                return a.edad - b.edad;
+            }
+        ));
+
+    if (criterio == "D")
+        console.log(arreglo.sort(
+            function(a, b) {
+                return b.edad - a.edad;
+            }
+        ));
+}
+
+ordenar_edad(personas, "D");
+
+
+// 3) Date
+
+var date = [
+    { nombre: "Luis", fecha: "1994/12/09" },
+    { nombre: "Cecilia", fecha: "1954/03/25" },
+    { nombre: "Maximiliano", fecha: "1977/08/02" },
+    { nombre: "Arya", fecha: "2015/10/19" },
+    { nombre: "Raúl", fecha: "1941/06/16" },
+    { nombre: "Esteban", fecha: "1965/02/11" },
+    { nombre: "Marcela", fecha: "1988/10/30" },
+];
+
+function ordenar_fecha(arreglo, criterio) {
+    if (criterio == "A")
+        console.log(arreglo.sort(
+            function(a, b) {
+                if (a.fecha > b.fecha) {
+                    return 1;
+                }
+                if (a.fecha < b.fecha) {
+                    return -1;
+                }
+                return 0;
+
+            }
+        ));
+
+    if (criterio == "D")
+        console.log(arreglo.sort(
+            function(a, b) {
+                if (a.fecha > b.fecha) {
+                    return -1;
+                }
+                if (a.fecha < b.fecha) {
+                    return 1;
+                }
+                return 0;
+            }));
+}
+
+ordenar_fecha(date, "A");
+
+
+
+// 4) Equipos
+
+class Equipo {
+    constructor(nombre, localidad, división) {
+        this.nombre = nombre;
+        this.localidad = localidad;
+        this.division = división;
+    }
+}
+
+function ordenar_equipo(arreglo, criterio) {
+    if (criterio == "A")
+        console.log(arreglo.sort(
+            function(a, b) {
+                if (a.nombre > b.nombre) {
+                    return 1;
+                }
+                if (a.nombre < b.nombre) {
+                    return -1;
+                }
+                return 0;
+
+            }
+        ));
+
+    if (criterio == "D")
+        console.log(arreglo.sort(
+            function(a, b) {
+                if (a.nombre > b.nombre) {
+                    return -1;
+                }
+                if (a.nombre < b.nombre) {
+                    return 1;
+                }
+                return 0;
+            }));
+}
+
+var equipo1 = new Equipo("Tigre", "Victoria", 2);
+var equipo2 = new Equipo("Excursionistas", "Belgrano", 4);
+var equipo3 = new Equipo("Newell's Old Boys", "Rosario", 1);
+var equipo4 = new Equipo("Morón", "Morón", 2);
+var equipo5 = new Equipo("River Plate", "Núñez", 1);
+var equipo6 = new Equipo("Los Andes", "Lomas de Zamora", 3);
+var equipo7 = new Equipo("Defensores de Cambaceres", "Ensenada", 5);
+
+
+var equipos = [equipo1, equipo2, equipo3, equipo4, equipo5, equipo6, equipo7];
+
+ordenar_equipo(equipos, "A");
+
+
+
+
+/*
+- - - - - - - DESAFÍO CLASE 6 - - - - - - -
 
 var gran_bretana = ["Londres", "Birmingham", "Nottingham", "Leeds", "Sheffield", "Cardiff", "Liverpool", "Manchester", "Newcastle", "Sunderland", "Glasgow"];
 var espana = ["Madrid", "Barcelona", "Sevilla", "Valencia", "Bilbao", "San Sebastián", "La Coruña", "Palma de Mallorca", "Tenerife", "Las Palmas"];
@@ -54,12 +218,11 @@ console.log(italia.length);
 
 
 
-/*
 
 - - - - - - - DESAFÍO CLASE 5 - - - - - - -
 
 class Viajero {
-    constructor(nombre, apellido, edad, tour) {
+    constructor(nombre, apellido, edad, tour, temporada) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
